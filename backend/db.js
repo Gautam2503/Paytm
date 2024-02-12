041368e98e487ae516ea58e5d3a46c7fc24c4450
Gautam2503 <gautamnaggn357@gmail.com>
2024-02-12 12:53:30 +0530

    Database updated

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoDB = "mongodb://gautamnaggn357:<password>@ac-ntgkvga-shard-00-00.p3ybjep.mongodb.net:27017,ac-ntgkvga-shard-00-01.p3ybjep.mongodb.net:27017,ac-ntgkvga-shard-00-02.p3ybjep.mongodb.net:27017/?replicaSet=atlas-13x2iq-shard-0&ssl=true&authSource=admin"
+const mongoDB = "mongodb://gautamnaggn357:Gautamcr735@ac-ntgkvga-shard-00-00.p3ybjep.mongodb.net:27017,ac-ntgkvga-shard-00-01.p3ybjep.mongodb.net:27017,ac-ntgkvga-shard-00-02.p3ybjep.mongodb.net:27017/?replicaSet=atlas-13x2iq-shard-0&ssl=true&authSource=admin"
 
 mongoose.connect(mongoDB).then(() => console.log("connected to MongoDb successfully")).catch((err) => console.log(err));
 
